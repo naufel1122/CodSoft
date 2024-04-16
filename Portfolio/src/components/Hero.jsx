9cdddf7e96e97d06ca0847dd800efbae2f194433
naufel1122 <nofil.khan037@gmail.com>
2024-04-16 18:58:53 +0500

    working

--- Portfolio/src/components/Hero.jsx
+++ Portfolio/src/components/Hero.jsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import { styles } from '../styles';
 import { navLinks } from '../constants';
 import { shaq, bwmap, worldmap  } from '../assets';
+// import { } from '../assets';
 
 const Hero = () => {
   return (
@@ -86,7 +87,7 @@ const Hero = () => {
             className="absolute bottom-0 ml-[50vw] 
             lg:ml-[75vw] md:ml-[60vw] xmd:ml-[60vw] 2xl:ml-[83vw]
             sm:h-[90vh] md:h-[70vh] xl:h-[80vh]"
-            src={shaq}
+            src={naufelnew}
             alt="shaquille"
           />
         </div> */}
