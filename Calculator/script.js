577ef97cae4bf19dddd3629f09c5950d1d050ec6
naufel1122 <nofil.khan037@gmail.com>
2024-04-23 16:30:49 +0500

    working

--- Calculator/script.js
+++ Calculator/script.js
@@ -1,90 +1,19 @@
-let runningTotal = 0;
-buffer = "0";
-let previousOperator;
 
-const screen = document.querySelector('.screen');
 
-function buttonClick(value) {
-    if (isNaN(value)) {
-        handleSymbol(value)
-    }
-    else {
-        handleNumber(value)
-    }
-    screen.innerText = buffer;
+const display = document.getElementById('display');
+ 
+function appendToDisplay(input) {
+    display.value += input;
 }
 
-function handleSymbol(symbol) {
-    switch (symbol) {
-        case 'C':
-            buffer = "0";
-            runningTotal = 0;
-            break;
-        case '=':
-            if (previousOperator === null) {
-                return
-            }
-            flushOperation(parseInt(buffer));
-            previousOperator = null;
-            buffer = runningTotal;
-            runningTotal = 0;
-            break;
-        case '←':
-            if (buffer.length === 1) {
-                buffer = '0';
-            }
-            else {
-                buffer = buffer.toString(0, buffer.length - 1)
-            }
-            break;
-        case '+':
-        case '-':
-        case '/':
-        case '*':
-            handleMath(symbol);
-            break;
-    }
+function clearDisplay() {
+    display.value = ''; 
 }
 
-function handleMath(symbol) {
-    if (buffer === '0') {
-        return;
-    }
-    const intBuffer = parseInt(buffer);
-    if (runningTotal === 0) {
-        runningTotal = intBuffer;
-    } else {
-        flushOperation(intBuffer);
-    }
-    previousOperator = symbol;
-    buffer = 0;
-}
-
-function flushOperation(intBuffer) {
-    if (previousOperator === '+') {
-        runningTotal += intBuffer;
-    }else if (previousOperator === '-') {
-        runningTotal -= intBuffer;
-    }else if (previousOperator === '×'){
-        runningTotal *= intBuffer;
-    }else if (previousOperator === '÷'){
-        runningTotal /= intBuffer;
+function calculate(){
+    try {
+        display.value = eval(display.value)
+    } catch (error) {
+        display.value = "Error";
     }
 }
-
-
-function handleNumber(numberString) {
-    if (buffer === '0') {
-        buffer = numberString
-    }else{
-        buffer += numberString;
-    }
-}
-
-function init() {
-    document.querySelector('.calc-buttons').addEventListener('click', function(event) {
-        buttonClick(event.target.innerText);
-    }) 
-}
-
-init();
